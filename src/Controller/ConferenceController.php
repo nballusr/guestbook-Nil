7ff9af9c00229ead0b58f602bfdf905e2ab78cc2
Nil Ballús <nballuri7@alumnes.ub.edu>
2023-03-15 14:23:14 +0100

    step 6.4.2

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,16 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
-    public function index(Request $request): Response
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name = ''): Response
     {
         $greet = '';
-        if ($name = $request->query->get('hello')) {
+        if ($name) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
 
-        //Començo pagina 64
-        //fer repo de git i guardar-lo i anar fent commits
         return new Response(<<<EOF
             <html>
                 <body>
